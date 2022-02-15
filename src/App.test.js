fce4a498b924f443d3f9f8ff43f201a59c3617b6
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-15 19:47:12 +0100

    :white_check_mark: tested delete button

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
-import { screen } from "@testing-library/react";
+import { screen, waitForElementToBeRemoved } from "@testing-library/react";
 import App from "./App";
 import { renderInStore } from "./setupTests";
+import userEvent from "@testing-library/user-event";
 
 describe("Given an App component", () => {
   describe("When it's rendered", () => {
@@ -12,6 +13,18 @@ describe("Given an App component", () => {
       const text = await screen.findByText(textToFind);
 
       expect(text).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered and clicked on the delete button from the third task with text 'todo-app", () => {
+    test("Then it should disapear", async () => {
+      const text = "todo-app";
+      renderInStore(<App />);
+      const tasks = await screen.findAllByRole("link", { name: /x/i });
+      userEvent.click(tasks[2]);
+      await waitForElementToBeRemoved(() => screen.queryByText(text));
+
+      expect(tasks[2]).not.toBeInTheDocument();
     });
   });
 });
